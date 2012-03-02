5105485005b286859861b6777c36f5730eaee00a
oncereply <oncereply@gmail.com>
2012-03-03 02:13:02 +0800

    compress javascript by using preferences settings

--- me.oncereply.jscompressor/src/me/oncereply/jscompressor/popup/actions/JavascriptCompressorAction.java
+++ me.oncereply.jscompressor/src/me/oncereply/jscompressor/popup/actions/JavascriptCompressorAction.java
@@ -1,27 +1,34 @@
 package me.oncereply.jscompressor.popup.actions;
 
-import java.lang.reflect.Method;
+import java.io.File;
 
 import me.oncereply.jscompressor.Activator;
+import me.oncereply.jscompressor.core.CompressorFactory;
+import me.oncereply.jscompressor.core.ICompressor;
+import me.oncereply.jscompressor.preferences.PreferenceConstants;
 import me.oncereply.jscompressor.util.ConsoleUtils;
+import me.oncereply.jscompressor.util.FileUtils;
+import me.oncereply.jscompressor.util.LogUtils;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.internal.PluginAction;
-
-import com.yahoo.platform.yui.compressor.YUICompressor;
 
 @SuppressWarnings("restriction")
 public class JavascriptCompressorAction implements IObjectActionDelegate {
 
 	private Shell shell;
+	private String compressorType;
+	private ICompressor compressor;
 
 	/**
 	 * Constructor for Action1.
@@ -44,6 +51,10 @@ public class JavascriptCompressorAction implements IObjectActionDelegate {
 		PluginAction opAction = (PluginAction) action;
 		ISelection selection = opAction.getSelection();
 		if ((selection instanceof TreeSelection)) {
+			ConsoleUtils.info("Compress starting...");
+			// 加载preferences配置
+			initPreferencesSetting();
+
 			Object element = ((TreeSelection) selection).getFirstElement();
 			if (element instanceof IResource) {
 				IResource resouce = (IResource) element;
@@ -58,25 +69,39 @@ public class JavascriptCompressorAction implements IObjectActionDelegate {
 					handleResource(resouce, path);
 				}
 			}
+			ConsoleUtils.info("Compress end.");
 		}
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private void handleResource(IResource resouce, String path) {
-		IFile file = (IFile) resouce;
-		ConsoleUtils.info("Compress successed: " + path);
-		try {
-			ClassLoader loader = Activator.getDefault().getClass()
-					.getClassLoader();
-			shell.getDisplay().getSyncThread();
-			Thread.currentThread().setContextClassLoader(loader);
-			Class c = loader.loadClass(YUICompressor.class.getName());
-			Method main = c.getMethod("main", new Class[] { String[].class });
-			String args[] = new String[] {
-					file.getLocation().toFile().getAbsolutePath(), "-o", path };
-			main.invoke(null, new Object[] { args });
-		} catch (Exception e) {
-			ConsoleUtils.error("Compress failed: " + path, e);
+	private void initPreferencesSetting() {
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		compressorType = store
+				.getString(PreferenceConstants.P_CHOICE_COMPRESSOR);
+		// 实例压缩对象
+		compressor = CompressorFactory.newCompressor(compressorType);
+	}
+
+	private void handleResource(IResource resource, String fullOutPath) {
+		// 输出文件路径
+		File temp = new File(fullOutPath);
+		if (temp.exists() || FileUtils.mkdirs(temp.getParentFile())) {
+			IFile file = (IFile) resource;
+			if (compressor != null) {
+				String args[] = new String[] {
+						file.getLocation().toFile().getAbsolutePath(), "-o",
+						fullOutPath };
+				try {
+					compressor.compress(args);
+					ConsoleUtils.info("successed: " + fullOutPath);
+				} catch (Exception e) {
+					ConsoleUtils.error("failed: " + fullOutPath, e);
+				}
+			}
+		} else {
+			// 创建文件不成功
+			LogUtils.error("File " + temp.getAbsolutePath()
+					+ " compress failed.");
+			return;
 		}
 	}
 
