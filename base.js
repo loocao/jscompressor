e1e9e0ed3f0dae5cc89fbf2cdafb7e83bf29704b
oncereply <oncereply@gmail.com>
2012-03-09 01:13:28 +0800

    new site and update version

--- base.js
+++ base.js
@@ -1,26 +1,18 @@
+var config = {
+	version : '2.0.1',
+	published : '2012-03-09',
+	copyright : '© 2012 - oncereply'
+};
 (function(window) {
-	var jc = window.jc = {
-		version : '1.0.0',
-		published : '2012-03-05'	
-	};
+	var jc = window.jc = config;
 	jc.init = function() {
-		var publised_date = 'Last Published: ' + jc.published + ' | Version: '
+		var publised_date = 'Last Updated: ' + jc.published + ' | Version: '
 				+ jc.version;
 		$('#publishDate').text(publised_date);
+		$('#footer').text(jc.copyright);
 	};
 })(window);
 
 window.onload = function() {
 	jc.init();
-	
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-29822122-1']);
-	_gaq.push(['_setDomainName', 'oncereply.me']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
 };
